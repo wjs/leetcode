b2a593b8b4bf93d36094c3a162193b39a006bcd0
unknown <jack.wei@DL00216-0760.DLOffice.avepoint.com>
2014-09-22 17:34:50 +0800

    update formart

--- python/same-tree.py
+++ python/same-tree.py
@@ -6,16 +6,16 @@
 #         self.right = None
 
 class Solution:
-    # @param p, a tree node
-    # @param q, a tree node
-    # @return a boolean
-    def isSameTree(self, p, q):
-        if (p != None and q != None):
-            if (p.val == q.val):
-                return ((self.isSameTree(p.left, q.left)) and (self.isSameTree(p.right, q.right)))
-            else:
-                return False
-        elif (p == None and q == None):
-            return True
-        else:
-            return False
+  # @param p, a tree node
+  # @param q, a tree node
+  # @return a boolean
+  def isSameTree(self, p, q):
+    if p != None and q != None:
+      if p.val == q.val:
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+      else:
+        return False
+    elif p == None and q == None:
+      return True
+    else:
+      return False
