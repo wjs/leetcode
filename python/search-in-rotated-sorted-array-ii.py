3f668d7e4b256912d0c74d96aff41e281da6a404
unknown <jack.wei@DL00216-0760.DLOffice.avepoint.com>
2014-10-06 17:00:31 +0800

    add a problem

---
+++ python/search-in-rotated-sorted-array-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+  # @param A, a list of integers
+  # @param target, an integer to be searched
+  # @return an integer
+  def search(self, A, target):
+    first = 0
+    last = len(A)
+    while first != last:
+      mid = (first + last) / 2
+      if (A[mid] == target):
+        return True
+      if A[first] < A[mid]:
+        if A[first] <= target and target < A[mid]:
+          last = mid
+        else:
+          first = mid + 1
+      elif A[first] > A[mid]:
+        if A[mid] < target and target <= A[last-1]:
+          first = mid + 1
+        else:
+          last = mid
+      else:
+        first += 1
+    return False
